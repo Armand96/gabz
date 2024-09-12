e3f5ca1ebe77e4bea5bcd83b8faf6e9af7cb2be9
Armand Iskandarsyah <armand.iskandarsyah@wingscorp.com>
2024-09-12 14:56:52 +0700

    ga jadi graphql

--- apps/api-gateway/src/api-gateway.module.ts
+++ apps/api-gateway/src/api-gateway.module.ts
@@ -3,7 +3,6 @@ import { ApiGatewayController } from './api-gateway.controller';
 import { ApiGatewayService } from './api-gateway.service';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { ProductsController } from './products/products.controller';
-import { OrderModule } from './order/order.module';
 
 @Module({
   imports: [
@@ -15,7 +14,6 @@ import { OrderModule } from './order/order.module';
     ClientsModule.register([
       { name: 'PRODUCT_SERVICE', transport: Transport.TCP, options: { host: 'localhost', port: 3001 } },
     ]),
-    OrderModule,
   ],
   controllers: [ApiGatewayController, ProductsController],
   providers: [ApiGatewayService],
