8e78762432906c1a41004a1221725a2cdffca07e
Armand Iskandarsyah <armand.iskandar96@gmail.com>
2024-09-22 23:55:49 +0700

    load balancing

--- apps/api-gateway/src/products/products.controller.ts
+++ apps/api-gateway/src/products/products.controller.ts
@@ -1,20 +1,34 @@
 import { CreateProductDto, Product, UpdateProductDto } from '@app/general';
 import { Body, Controller, Delete, Get, Inject, Param, Post, Put } from '@nestjs/common';
-import { ClientProxy } from '@nestjs/microservices';
+import { ClientProxy, ClientProxyFactory, Transport } from '@nestjs/microservices';
 import { catchError, map, Observable, of } from 'rxjs';
 import { ResponseDto } from '../response.dto';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('products')
 export class ProductsController {
 
     constructor(
-        @Inject('PRODUCT_SERVICE') private client: ClientProxy,
+        // @Inject('PRODUCT_SERVICE') private client: ClientProxy, // use this for persistent TCP connection (load balancing is kinda hard)
+        private configService: ConfigService,
         private res: ResponseDto<Product>,
     ) { }
 
+    // use this for close connection TCP, so the docker can load balance it since it is not persistence TCP connection
+    private createClient(): ClientProxy {
+        return ClientProxyFactory.create({
+            transport: Transport.TCP,
+            options: {
+                host: this.configService.get('PRODUCT_HOST'),
+                port: this.configService.get('PRODUCT_PORT'),
+            },
+        });
+    }
+
     @Get()
     async getAll() {
-        return this.client.send('getAllProduct', {}).pipe(
+        const client = this.createClient();
+        return client.send('getAllProduct', {}).pipe(
             map(this.res.mapData),
             catchError(this.res.handleError)
         );
@@ -22,22 +36,26 @@ export class ProductsController {
 
     @Get(':id')
     getOneProduct(@Param('id') id: number) {
-        return this.client.send('getOneProduct', id).pipe(map(this.res.mapData), catchError(this.res.handleError));
+        const client = this.createClient();
+        return client.send('getOneProduct', id).pipe(map(this.res.mapData), catchError(this.res.handleError));
     }
 
     @Post()
     createProduct(@Body() createProductDto: CreateProductDto) {
-        return this.client.send('createProduct', createProductDto).pipe(map(this.res.mapData), catchError(this.res.handleError));
+        const client = this.createClient();
+        return client.send('createProduct', createProductDto).pipe(map(this.res.mapData), catchError(this.res.handleError));
     }
 
     @Put(':id')
     updateProduct(@Param('id') id: number, @Body() updateProductDto: UpdateProductDto) {
+        const client = this.createClient();
         // console.log(id);
-        return this.client.send('updateProduct', { id: id, updateProductDto: updateProductDto }).pipe(map(this.res.mapData), catchError(this.res.handleError));
+        return client.send('updateProduct', { id: id, updateProductDto: updateProductDto }).pipe(map(this.res.mapData), catchError(this.res.handleError));
     }
 
     @Delete(':id')
     deleteProduct(@Param('id') id: number) {
-        return this.client.send('deleteProduct', id).pipe(map(this.res.mapData), catchError(this.res.handleError));
+        const client = this.createClient();
+        return client.send('deleteProduct', id).pipe(map(this.res.mapData), catchError(this.res.handleError));
     }
 }
